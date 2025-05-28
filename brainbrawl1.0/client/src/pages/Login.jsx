abce88fb10647d0f808a53a18c6cb7ddbd87d563
NinJaWay <130268824+heeelol@users.noreply.github.com>
2025-05-28 16:03:46 +0800

    added required field in login and registration form

--- brainbrawl1.0/client/src/pages/Login.jsx
+++ brainbrawl1.0/client/src/pages/Login.jsx
@@ -34,9 +34,9 @@ export default function Login() {
         <div>
             <form onSubmit={loginUser}>
                 <label>Email</label>
-                <input type="email" placeholder="Enter your email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+                <input type="email" placeholder="Enter your email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} required/>
                 <label>Password</label>
-                <input type="password" placeholder="Enter your password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+                <input type="password" placeholder="Enter your password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} required />
                 <button type='submit'>Submit</button>
             </form>
         </div>
--- brainbrawl1.0/client/src/pages/Register.jsx
+++ brainbrawl1.0/client/src/pages/Register.jsx
@@ -37,9 +37,9 @@ export default function Register() {
         <div>
             <form onSubmit={registerUser}>
                 <label>Name</label>
-                <input type="text" placeholder="Enter your name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})}  />
+                <input type="text" placeholder="Enter your name" value={data.name} onChange={(e) => setData({...data, name: e.target.value})} required />
                 <label>Email</label>
-                <input type="email" placeholder="Enter your email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}  />
+                <input type="email" placeholder="Enter your email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} required />
                 <label>Password</label>
                 <input type="password" placeholder="Enter your password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})}  />
                 <button type='submit'>Submit</button>
