d39c3dd0b3386b0d2c24f9acb67be9a1f2c448a3
NinJaWay <yaporx@gmail.com>
2025-06-12 16:06:58 +0800

    updated vercel URL

--- brainbrawl1.0/client/src/config/api.js
+++ brainbrawl1.0/client/src/config/api.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.PROD 
-  ? 'https://brainbrawl-backend-bw7x.onrender.com'
-  : 'http://localhost:8000';
+  ? 'https://brainbrawl-backend-bw7x.onrender.com' //Production URL
+  : 'http://localhost:8000'; //Development URL
 
 export default API_URL;
